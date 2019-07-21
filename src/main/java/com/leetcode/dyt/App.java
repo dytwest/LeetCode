3dd4acf01dbb766593ca5c26386dd2aa2b325848
dytwest <dytwest@163.com>
2019-07-21 12:05:20 +0800

    add more ignore files

--- src/main/java/com/leetcode/dyt/App.java
+++ src/main/java/com/leetcode/dyt/App.java
@@ -6,8 +6,7 @@ package com.leetcode.dyt;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("hello world");
     }
 }
